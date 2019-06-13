bc6d52f1030d9f3e07bbd2cb523413dd9dbf3ac2
devkonstan <konrad.stankievich@gmail.com>
2019-06-13 15:40:49 +0200

    first commit

---
+++ src/Files/ExecutableFile.java
@@ -0,0 +1,24 @@
+package Files;
+
+public class ExecutableFile extends SystemFile{
+
+    public ExecutableFile(String name) {
+        super(name);
+    }
+
+    @Override
+    protected String getSystemFileName() {
+        return name;
+    }
+
+
+    @Override
+    public void addFile(SystemFile sf) {
+
+    }
+
+    @Override
+    public void removeFile(SystemFile sf) {
+
+    }
+}
